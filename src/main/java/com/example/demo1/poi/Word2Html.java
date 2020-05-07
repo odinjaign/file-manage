79d647f73766b853d2fb66cdf6a0234b6fa896bc
jaign <jaign@outlook.com>
2020-05-07 22:13:07 +0800

    解决doc2html

--- src/main/java/com/example/demo1/poi/Word2Html.java
+++ src/main/java/com/example/demo1/poi/Word2Html.java
@@ -2,29 +2,27 @@ package com.example.demo1.poi;
 
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.URLUtil;import org.apache.poi.hwpf.HWPFDocument;
+import cn.hutool.core.util.URLUtil;
+import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.converter.PicturesManager;
 import org.apache.poi.hwpf.converter.WordToHtmlConverter;
-import org.apache.poi.hwpf.usermodel.Picture;
 import org.apache.poi.hwpf.usermodel.PictureType;
 import org.apache.poi.xwpf.converter.core.BasicURIResolver;
 import org.apache.poi.xwpf.converter.core.FileImageExtractor;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLConverter;
 import org.apache.poi.xwpf.converter.xhtml.XHTMLOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-
 import org.w3c.dom.Document;
-
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-
-
 import java.io.*;
-import java.util.List;
 
 
 public class Word2Html {
@@ -35,72 +33,28 @@ public class Word2Html {
 	/**
 	 * doc转换为html
 	 */
-	public static void doc2Html(String fileName, String outPutFile) throws TransformerException, IOException, ParserConfigurationException, TransformerException, ParserConfigurationException {
-//		long startTime = System.currentTimeMillis();
-//		HWPFDocument wordDocument = new HWPFDocument(new FileInputStream(fileName));
-//		WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
-//		wordToHtmlConverter.setPicturesManager(new PicturesManager() {
-//			public String savePicture(byte[] content, PictureType pictureType, String suggestedName, float widthInches, float heightInches) {
-//				return "test/" + suggestedName;
-//			}
-//		});
-//		wordToHtmlConverter.processDocument(wordDocument);
-//		// 保存图片
-//		List<Picture> pics = wordDocument.getPicturesTable().getAllPictures();
-//		if (pics != null) {
-//			for (int i = 0; i < pics.size(); i++) {
-//				Picture pic = (Picture) pics.get(i);
-//				System.out.println("Pic");
-//
-//				try {
-//					pic.writeImageContent(new FileOutputStream(tempPath + pic.suggestFullFileName()));
-//				} catch (FileNotFoundException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}
-//		Document htmlDocument = wordToHtmlConverter.getDocument();
-//		ByteArrayOutputStream out = new ByteArrayOutputStream();
-//		DOMSource domSource = new DOMSource(htmlDocument);
-//		StreamResult streamResult = new StreamResult(out);
-//
-//		TransformerFactory tf = TransformerFactory.newInstance();
-//		Transformer serializer = tf.newTransformer();
-//		serializer.setOutputProperty(OutputKeys.ENCODING, "utf-8");
-//		serializer.setOutputProperty(OutputKeys.INDENT, "yes");
-//		serializer.setOutputProperty(OutputKeys.METHOD, "html");
-//		serializer.transform(domSource, streamResult);
-//		out.close();
-//		String content = new String(out.toByteArray());
-//		//content = content.replace("<img src=\"", "<img src=\"" + imgurlPre+tempPath+ "/");
-//		writeFile(content, outPutFile);
-//		System.out.println("Generate " + outPutFile + " with " + (System.currentTimeMillis() - startTime) + " ms.");
-
+	public static void doc2Html(String fileName, String outPutFile) throws IOException, TransformerException, ParserConfigurationException {
 		InputStream input = new FileInputStream(fileName);
 		HWPFDocument wordDocument = new HWPFDocument(input);
 		Document newDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
 		WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(newDocument);
+		String imgTempFolder = tempPath + "temp/";
+		FileUtil.mkdir(imgTempFolder);
 		wordToHtmlConverter.setPicturesManager(new PicturesManager() {
-			public String savePicture(byte[] bytes, PictureType pictureType, String s, float v, float v1) {
-				System.out.println(s);
-				return s;
+			public String savePicture(byte[] bytes, PictureType pictureType, String name, float v, float v1) {
+
+				File img = new File(imgTempFolder, name);
+				try {
+					FileOutputStream stream = new FileOutputStream(img);
+					stream.write(bytes);
+					stream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return URLUtil.encode(img.getPath());
 			}
 		});
 		wordToHtmlConverter.processDocument(wordDocument);
-		List pics = wordDocument.getPicturesTable().getAllPictures();
-		String imgTmpPath = tempPath + "temp/";
-		FileUtil.mkdir(imgTmpPath);
-		if (pics != null) {
-			for (Object pic1 : pics) {
-				Picture pic = (Picture) pic1;
-				try {
-					String name = pic.suggestFullFileName();
-					pic.writeImageContent(new FileOutputStream(imgTmpPath + pic.suggestFullFileName()));
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			}
-		}
 		Document document = wordToHtmlConverter.getDocument();
 		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
 		DOMSource domSource = new DOMSource(document);
@@ -113,9 +67,7 @@ public class Word2Html {
 		serializer.transform(domSource, streamResult);
 		outStream.close();
 		String content = new String(outStream.toByteArray());
-		content = content.replace("<img src=\"", "<img src=\"" + imgurlPre+ imgTmpPath + "");
-
-//		FileUtils.writeStringToFile(new File(outPutFile), content, "utf-8");
+		content = content.replace("<img src=\"", "<img src=\"" + imgurlPre + "");
 		FileUtil.writeString(content,new File(outPutFile),"utf-8");
 	}
  
